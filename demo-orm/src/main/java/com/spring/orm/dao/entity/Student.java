4bc58319c6b770f0ed150410c2ab9033b97eac5e
lixin <hgllyppl@yeah.net>
2019-08-20 17:50:34 +0800

    [update] 增加 mybatis 插件

--- demo-orm/src/main/java/com/spring/orm/dao/entity/Student.java
+++ demo-orm/src/main/java/com/spring/orm/dao/entity/Student.java
@@ -11,48 +11,43 @@ public class Student {
 
     private Integer age;
 
-    public String getAddress() {
-        return address;
-    }
-
-    public Student setAddress(String address) {
-        this.address = address;
-        return this;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public Student setAge(Integer age) {
-        this.age = age;
-        return this;
-    }
-
     public Integer getId() {
         return id;
     }
 
-    public Student setId(Integer id) {
+    public void setId(Integer id) {
         this.id = id;
-        return this;
     }
 
     public String getName() {
         return name;
     }
 
-    public Student setName(String name) {
-        this.name = name;
-        return this;
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address == null ? null : address.trim();
     }
 
     public Integer getSex() {
         return sex;
     }
 
-    public Student setSex(Integer sex) {
+    public void setSex(Integer sex) {
         this.sex = sex;
-        return this;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
     }
 }
